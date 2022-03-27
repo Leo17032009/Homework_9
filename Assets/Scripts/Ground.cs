ddbc8ad5220376fefb0e5e5dbc8781bb1eebd7b7
Leo17032009 <89399852+Leo17032009@users.noreply.github.com>
2022-03-27 18:19:06 +0300

    init

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ground : MonoBehaviour 
+{
+	[SerializeField] private Text _gameHealth;
+	[SerializeField] private GameObject _losePanel;
+	[SerializeField] private GameObject _gamePanel;
+	private int _health;
+
+	private void Start()
+	{
+		_health = 5;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag ("GoodFood") || collision.gameObject.CompareTag ("BadFood")) 
+		{
+			Destroy (collision.gameObject);
+			if (collision.gameObject.CompareTag ("GoodFood") == true) 
+			{
+				_health = _health - 1;
+			}
+			_gameHealth.text = _health.ToString ();
+			if (_health <= 0) {
+				_gamePanel.SetActive (false);
+				_losePanel.SetActive (true);
+				Time.timeScale = 0;
+			}
+		}
+	}
+}
